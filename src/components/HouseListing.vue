7748728602fb564434493ae7379a3271e993f55a
Paul Fontes <pgfon22@gmail.com>
2025-09-17 14:35:13 -0600

    finishing house template

---
+++ src/components/HouseListing.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { House } from '@/models/House.js';
+
+const props = defineProps({
+    houseProp: {type: House, required: true}
+})
+
+</script>
+
+
+<template>
+    <div class="house-border">
+        <img :src="houseProp.imgUrl" alt="" class="house-img">
+        <p>Bathrooms: {{ houseProp.bathrooms }}</p>
+        <p>Bedrooms: {{ houseProp.bedrooms }}</p>
+        <p>Asking Price: {{ houseProp.price }}</p>
+        <p>Year Built: {{ houseProp.year }}</p>
+        <p>House Levels: {{ houseProp.levels }}</p>
+        <p>{{ houseProp.description }}</p>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.house-img{
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+}
+
+.house-border{
+    height: 650px;
+    border: 8px inset black;
+}
+</style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import HouseListing from '@/components/HouseListing.vue';
 import { housesService } from '@/services/HousesService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
@@ -24,7 +25,13 @@ async function getHouses(){
 
 
 <template>
-{{ houses }}
+    <div class="container-fluid mt-5">
+        <div class="row g-3">
+            <div v-for="house in houses" :key="house.id" class="col-md-6">
+                <HouseListing :houseProp="house" />
+            </div>
+        </div>
+    </div>
 </template>
 
 
